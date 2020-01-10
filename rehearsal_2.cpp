8a0a83f9b207eddfe45f00ddc94c0d88d7f6d3ab
unknown <sunantha1881@gmail.com>
2020-01-10 17:16:57 +0700

    test4

--- rehearsal_2.cpp
+++ rehearsal_2.cpp
@@ -1,1 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float sum=0, x;
+    
+    cout<<"Enter x : ";
+    cin>>x; 
+    if(x==0){
+    cout<<"sum = "<<sum;}else
+    {
+         while (x==0){
+    if(x>0){
+
+        sum=sum+x;
+    }
+        cout<<"Enter x : ";
+        cin>>x;   
+         } cout<<"sum = "<<sum;    
+  
+   return 0;
+} 
+}
+    
+
+    
+        
+         
+        
+
+           
+
+
 
