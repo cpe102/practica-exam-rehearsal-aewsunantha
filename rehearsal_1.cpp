308ba01e7a8ba14ee34697bf4353219d2d2ead3f
unknown <sunantha1881@gmail.com>
2020-01-10 16:14:14 +0700

    test1

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+int main(){
+    string Name;
+    string text1=" InW Jrim Jrim!!!";
+    string text2="Try harder, ";
+    float x;
+    cout<<"What is your name ? : ";
+    cin>>Name;
+    cout<<"What i your GPA ? : ";
+    cin>> x;
+    if(x>=3.5){
+        cout <<Name+text1;
 
+    }else{
+        cout<<text2+Name<<"!!!";
+    }
+return 0;
+
+}
